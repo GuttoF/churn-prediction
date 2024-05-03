94afa6a1873e866dfb856faa1209d250dbab789b
“Gutto <guttolaudie@gmail.com>
2024-05-03 13:54:46 -0300

    feat: finished eda

--- churn_prediction/helpers/classes/DataVisualizer.py
+++ churn_prediction/helpers/classes/DataVisualizer.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import scipy.stats as ss
 from plotly.subplots import make_subplots
+from scipy.stats import chi2_contingency
 
 
 class DataVisualizer:
@@ -16,7 +17,20 @@ class DataVisualizer:
         """
         self.data = data
 
-    def multiple_distplots(self, columns: list, colors: list = None) -> None:
+    colors_list = [
+        "#1D5B79",
+        "#1D7865",
+        "#78621D",
+        "#784F1D",
+        "#1B2F38",
+        "#75CDBB",
+        "#CDB875",
+        "#CDA675",
+        "#B7E2F7",
+        "#E4FFF9",
+    ]
+
+    def multiple_distplots(self, columns: list) -> None:
         """
         Create multiple distribution plots (distplots) with histogram and KDE on the same scale.
 
@@ -34,10 +48,6 @@ class DataVisualizer:
                 cols += 1
 
         fig = make_subplots(rows=rows, cols=cols, subplot_titles=columns)
-
-        if colors is None:
-            colors = ["blue"] * num_columns
-
         row, col = 1, 1
 
         for i, column in enumerate(columns):
@@ -48,7 +58,7 @@ class DataVisualizer:
                     name=column + " Histogram",
                     nbinsx=30,
                     opacity=0.75,
-                    marker_color=colors[i],
+                    marker_color=self.colors_list[i],
                     histnorm="probability density",
                 ),
                 row=row,
@@ -65,7 +75,7 @@ class DataVisualizer:
                     y=kde_y,
                     mode="lines",
                     name=column + " KDE",
-                    line=dict(color=colors[i], width=2),
+                    line=dict(color=self.colors_list[i], width=2),
                 ),
                 row=row,
                 col=col,
@@ -81,22 +91,18 @@ class DataVisualizer:
         )
         fig.show()
 
-    def multiple_barplots(self, columns: list, colors: list = None) -> None:
+    def multiple_barplots(self, columns: list) -> None:
         """
         Generate multiple bar plots for specified columns in the DataFrame.
 
         Args:
         - columns (list): A list of column names to be plotted.
-        - colors (list, optional): A list of colors to be used for the bar plots. Defaults to blue.
         """
         num_columns = len(columns)
         cols = int(num_columns**0.5)
         rows = (num_columns + cols - 1) // cols
 
         fig = make_subplots(rows=rows, cols=cols, subplot_titles=columns)
-
-        if colors is None:
-            colors = ["blue"] * num_columns
 
         index = 0
         for row in range(1, rows + 1):
@@ -110,7 +116,9 @@ class DataVisualizer:
                             x=column_data["category"],
                             y=column_data["count"],
                             name=columns[index],
-                            marker_color=colors[index % len(colors)],
+                            marker_color=self.colors_list[
+                                index % len(self.colors_list)
+                            ],
                         ),
                         row=row,
                         col=col,
@@ -165,9 +173,19 @@ class DataVisualizer:
                 self.data, dimensions=columns, color=self.data[color_column]
             )
         else:
-            fig = px.scatter_matrix(self.data, dimensions=columns)
+            fig = px.scatter_matrix(
+                self.data,
+                dimensions=columns,
+                color_discrete_sequence=self.colors_list,
+            )
 
-        fig.update_layout(title_text="Scatter Plot Matrix", height=1000, width=1000)
+        fig.update_layout(
+            title_text="Scatter Plot Matrix",
+            height=1000,
+            width=1000,
+            xaxis_tickangle=-45,
+            yaxis_tickangle=-45,
+        )
         fig.show()
 
     def cramers_v(self, x, y):
@@ -227,4 +245,395 @@ class DataVisualizer:
             width=1200,
             annotations=annotations,
         )
+        fig.show()
+
+    def statistic_test(self, featureA: str, featureB: str) -> None:
+        """
+        Perform a chi-square test of independence between two categorical variables.
+
+        Parameters:
+        - featureA: str
+            The name of the first categorical variable.
+        - featureB: str
+            The name of the second categorical variable.
+
+        Returns:
+        None
+
+        Prints the chi-square statistic, p-value, and the result of the hypothesis test.
+        """
+        table = pd.crosstab(self.data[featureA], self.data[featureB])
+        result = chi2_contingency(table)
+        print(
+            f"Chi2 Statistic: {round(result.statistic, 3)}\nP-value: {round(result.pvalue, 3)}"
+        )
+
+        if result.pvalue < 0.05:
+            print("Reject the null hypothesis: The variables are dependent")
+        else:
+            print("Fail to reject the null hypothesis: The variables are independent")
+
+    def hypotheses_1(self) -> None:
+        credit_score_mean = self.data["credit_score"].mean()
+
+        self.data["credit_score_category"] = self.data["credit_score"].apply(
+            lambda x: "Above Average" if x >= credit_score_mean else "Below Average"
+        )
+
+        aux = self.data.groupby("credit_score_category")["exited"].mean().reset_index()
+
+        title = "Churn Rate by Credit Score Category<br>"
+        suptitle = "<sub>Shows the percentage of customers who <b>exited</b> based on their credit score being above or below average</sub>"
+        info = title + suptitle
+
+        fig = px.bar(
+            aux,
+            y="credit_score_category",
+            x="exited",
+            title=info,
+            hover_data={"exited": True, "credit_score_category": False},
+            labels={
+                "exited": "Churn Rate",
+                "credit_score_category": "Credit Score Category",
+            },
+            color="credit_score_category",
+            color_discrete_sequence=[self.colors_list[0], self.colors_list[1]],
+            barmode="group",
+            text_auto=".2%",
+        )
+
+        fig.update_layout(
+            xaxis_title="Credit Score Category",
+            yaxis_title="Churn Rate",
+            plot_bgcolor="white",
+            paper_bgcolor="white",
+            width=800,
+            height=400,
+        )
+
+        fig.show()
+
+    def hypotheses_2(self) -> None:
+        self.data["group_product"] = self.data["num_of_products"].astype(str)
+
+        self.data.sort_values("num_of_products", ascending=True, inplace=True)
+
+        title = "Product Usage by Age Group<br>"
+        suptitle = "<sub>How many products does each age group have?</sub>"
+        info = title + suptitle
+
+        fig = px.bar(
+            self.data,
+            x="life_stage",
+            y="num_of_products",
+            title=info,
+            hover_data={
+                "num_of_products": True,
+                "life_stage": False,
+                "group_product": False,
+            },
+            labels={
+                "num_of_products": "Number of Products",
+                "life_stage": "Age Group",
+            },
+            color="group_product",
+            color_discrete_sequence=[
+                self.colors_list[0],
+                self.colors_list[1],
+                self.colors_list[2],
+                self.colors_list[3],
+            ],
+            barmode="group",
+            text_auto=False,
+        )
+
+        legend_order = {"1": 1, "2": 2, "3": 3, "4": 4}
+
+        for trace in fig.data:
+            if trace.name in legend_order:
+                trace.legendgroup = trace.name
+                trace.legendrank = legend_order[trace.name]
+
+        fig.update_yaxes(dtick=500)
+
+        fig.update_layout(
+            legend_title="Number of Products",
+            yaxis_title="Total",
+            xaxis_title="Age Group",
+            plot_bgcolor="white",
+            paper_bgcolor="white",
+            width=800,
+            height=400,
+        )
+
+        fig.show()
+
+    def hypotheses_3(self) -> None:
+        aux = self.data.groupby("geography")["balance"].mean().reset_index()
+
+        aux.sort_values("balance", ascending=False, inplace=True)
+
+        title = "Average Account Balance by Region<br>"
+        suptitle = "<sub>What is the average balance on each country?</sub>"
+        info = title + suptitle
+
+        fig = px.bar(
+            aux,
+            y="geography",
+            x="balance",
+            title=info,
+            hover_data={"balance": True, "geography": False},
+            labels={"balance": "Average Account Balance", "geography": "Region"},
+            color="geography",
+            color_discrete_sequence=[
+                self.colors_list[0],
+                self.colors_list[1],
+                self.colors_list[2],
+            ],
+            barmode="group",
+            text_auto=True,
+        )
+
+        fig.update_layout(
+            xaxis_title="Average Account Balance",
+            yaxis_title="Region",
+            plot_bgcolor="white",
+            paper_bgcolor="white",
+            width=800,
+            height=400,
+        )
+
+        fig.show()
+
+    def hypotheses_4(self) -> None:
+        active_members = self.data[self.data["is_active_member"] == 1]
+        gender_counts = active_members["gender"].value_counts().reset_index()
+        gender_counts.columns = ["gender", "count"]
+
+        active_by_gender = (
+            self.data.groupby("gender")["is_active_member"].mean().reset_index()
+        )
+        active_by_gender.sort_values("is_active_member", ascending=False, inplace=True)
+
+        colors_map = {"Male": self.colors_list[0], "Female": self.colors_list[1]}
+
+        fig = make_subplots(
+            rows=1,
+            cols=2,
+            specs=[[{"type": "bar"}, {"type": "pie"}]],
+            subplot_titles=(
+                "Proportion of Active Members by Gender",
+                "Gender Proportion",
+            ),
+        )
+
+        bar = px.bar(
+            active_by_gender,
+            x="gender",
+            y="is_active_member",
+            color="gender",
+            color_discrete_map=colors_map,
+            text_auto=".2%",
+            labels={
+                "is_active_member": "Active Members Proportion",
+                "gender": "Gender",
+            },
+        )
+
+        for trace in bar.data:
+            # trace.width = [1] * len(active_by_gender)
+            fig.add_trace(trace, row=1, col=1)
+
+        pie = px.pie(
+            gender_counts,
+            values="count",
+            names="gender",
+            color="gender",
+            color_discrete_map=colors_map,
+        )
+
+        for trace in pie.data:
+            trace.showlegend = False
+            fig.add_trace(trace, row=1, col=2)
+
+        fig.update_layout(
+            title_text="What is the most active gender?",
+            plot_bgcolor="white",
+            paper_bgcolor="white",
+            width=800,
+            height=400,
+            legend_title_text="Gender",
+            # xaxis_tickangle=90,
+        )
+
+        fig.show()
+
+    def hypotheses_5(self) -> None:
+        self.data["salary_category"] = pd.qcut(
+            self.data["estimated_salary"],
+            q=4,
+            labels=["Low", "Medium", "High", "Very High"],
+        )
+
+        aux = (
+            self.data.groupby("salary_category", observed=False)["has_cr_card"]
+            .mean()
+            .reset_index()
+        )
+
+        aux.sort_values("has_cr_card", ascending=False, inplace=True)
+
+        title = "Credit Card Ownership by Salary Category<br>"
+        suptitle = "<sub>What is the proportion of customers with a credit card in each salary category?</sub>"
+        info = title + suptitle
+
+        fig = px.bar(
+            aux,
+            y="salary_category",
+            x="has_cr_card",
+            title=info,
+            color="salary_category",
+            color_discrete_sequence=self.colors_list,
+            labels={
+                "has_cr_card": "Credit Card Ownership",
+                "salary_category": "Salary Category",
+            },
+            barmode="group",
+            text_auto=".2%",
+        )
+
+        fig.update_traces(width=[0.6] * len(aux))
+
+        fig.update_layout(
+            yaxis_title="Salary Category",
+            xaxis_title="Credit Card Ownership",
+            plot_bgcolor="white",
+            paper_bgcolor="white",
+            width=800,
+            height=400,
+        )
+
+        fig.show()
+
+    def hypotheses_6(self) -> None:
+        aux = self.data.groupby("tenure")["exited"].mean().reset_index()
+
+        title = "Churn Rate by Tenure<br>"
+        suptitle = "<sub>Is there a relationship between <b>churn</b> and tenure?</sub>"
+        info = title + suptitle
+
+        aux.sort_values("tenure", ascending=True, inplace=True)
+
+        fig = px.line(
+            aux,
+            x="tenure",
+            y="exited",
+            hover_data={"exited": True, "tenure": False},
+            title=info,
+            color_discrete_sequence=self.colors_list,
+            markers=False,
+        )
+
+        fig.update_traces(
+            line=dict(width=4),
+        )
+
+        fig.update_xaxes(tickvals=aux["tenure"].unique())
+
+        fig.update_layout(
+            xaxis_title="Tenure",
+            yaxis_title="Churn Rate",
+            plot_bgcolor="white",
+            paper_bgcolor="white",
+            width=800,
+            height=400,
+        )
+
+        fig.show()
+
+    def hypotheses_7(self) -> None:
+        aux = self.data.groupby("balance_indicator")["exited"].mean().reset_index()
+
+        aux.sort_values("balance_indicator", ascending=True, inplace=True)
+
+        title = "Churn rate by Balance Category<br>"
+        suptitle = "<sub>Is there a significant difference in the churn rate among customers with different account balances?</sub>"
+        info = title + suptitle
+
+        fig = px.bar(
+            aux,
+            y="balance_indicator",
+            x="exited",
+            title=info,
+            hover_data={"balance_indicator": False, "exited": True},
+            labels={
+                "balance_indicator": "Balance Indicator",
+                "exited": "Churn Rate",
+            },
+            color="balance_indicator",
+            color_discrete_sequence=[
+                self.colors_list[0],
+                self.colors_list[1],
+                self.colors_list[2],
+            ],
+            barmode="group",
+            text_auto=".2%",
+        )
+
+        fig.update_layout(
+            yaxis_title="Balance",
+            xaxis_title="Churn Rate",
+            plot_bgcolor="white",
+            paper_bgcolor="white",
+            width=800,
+            height=400,
+        )
+
+        fig.show()
+
+    def hypotheses_8(self) -> None:
+        aux = (
+            self.data.groupby("life_stage", observed=False)["has_cr_card"]
+            .mean()
+            .reset_index()
+        )
+
+        aux.sort_values("life_stage", ascending=True, inplace=True)
+
+        title = "Credit_Card Ownership by Age<br>"
+        suptitle = "<sub>Is there a significant difference in the Credit Card Ownership among customers with different age?</sub>"
+        info = title + suptitle
+
+        fig = px.bar(
+            aux,
+            y="life_stage",
+            x="has_cr_card",
+            title=info,
+            hover_data={"life_stage": True, "has_cr_card": False},
+            labels={
+                "life_stage": "Age Group",
+                "has_cr_card": "Credit Card Ownership",
+            },
+            color="life_stage",
+            color_discrete_sequence=[
+                self.colors_list[0],
+                self.colors_list[1],
+                self.colors_list[2],
+                self.colors_list[3],
+            ],
+            barmode="group",
+            text_auto=".2%",
+        )
+
+        fig.update_traces(width=[0.6] * len(aux))
+
+        fig.update_layout(
+            yaxis_title="Age Group",
+            xaxis_title="Credit Card Ownership",
+            plot_bgcolor="white",
+            paper_bgcolor="white",
+            width=800,
+            height=400,
+        )
+
         fig.show()
